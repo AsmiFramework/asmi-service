dbad1d3faa1cfb3d71dd3726b63a832572f6ace0
Mahesh J <mahesh.j567@gmail.com>
2016-10-21 11:37:22 +0530

    first commit

---
+++ asmi-service.js
@@ -0,0 +1,37 @@
+var util = require('util');
+var	config = require('config');
+
+function AsmiService(modName) {
+	var root = this;
+
+	this.conf = config.modules[modName] || {};
+	if (this.conf.protocolOpts) {
+		protocol = this.conf.protocolOpts.protocol || null;
+	} else {
+		protocol = null;
+	}
+	if (!protocol && config.modules["asmi-service"]) {
+		protocol = config.modules["asmi-service"].protocol || "direct";
+	} else {
+		protocol = "direct";
+	}
+
+	protocolServer = require('asmi-protocol-' + protocol)(modName);
+	protocolServer.initServer(this);
+}
+
+var p = AsmiService.prototype;
+
+p.getInstanceID = function () {
+	return this.instanceID;
+};
+
+// properties not declared on prototype (var x) will behave like static variables, so sub classes of this class will have these as static variables
+p.conf = null;
+p.modName = null;
+p.mod = null;
+p.instanceID = null;
+
+module.exports = exports = function (modpath) {
+	return new AsmiService(modpath);
+};
